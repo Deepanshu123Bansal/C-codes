df7848a4881f0ee3c49a9f1d8601f7ea2eb11a1d
Deepanshu123Bansal <122732006+Deepanshu123Bansal@users.noreply.github.com>
2023-01-15 19:02:18 +0530

    Update armstrong number.c

--- armstrong number.c
+++ armstrong number.c
@@ -1,1 +1,20 @@
-includes
+#include<stdio.h>
+int main()
+{
+  int n,i,count=0;
+  printf("enter the n");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    if(n%i==0)
+      count++;
+  }
+  if(count==2)
+  {
+    printf("armstrong number");
+  }
+  else
+  {
+    printf("not armstrong");
+  }
+}
