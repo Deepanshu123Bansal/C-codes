ae61b2aaa06bd97dd9546d987774bfad999b42d1
Deepanshu123Bansal <122732006+Deepanshu123Bansal@users.noreply.github.com>
2023-03-25 01:05:45 +0530

    Create Area_parallogram.c

---
+++ Area_parallogram.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int base,height,area;
+  printf("Enter base ,height");
+  scanf("%d%d",&base,&height);
+  area=base*height;
+  printf("%d",area);
+  return 0;
+}
+  
