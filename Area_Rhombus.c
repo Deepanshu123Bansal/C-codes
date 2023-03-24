26e15788789d042259fee4465e13ba9b49cb6002
Deepanshu123Bansal <122732006+Deepanshu123Bansal@users.noreply.github.com>
2023-03-25 01:12:50 +0530

    Create Area_Rhombus.c

---
+++ Area_Rhombus.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int height,side,rhombus;
+  printf("enter height,side");
+  scanf("%d%d",&height,&side);
+  rhombus=height*side;
+  printf("%d",rhombus);
+  return 0;
+}
