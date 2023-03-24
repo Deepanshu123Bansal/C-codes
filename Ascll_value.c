26496cb88c9ecb4416966b4619fada1142d97f43
Deepanshu123Bansal <122732006+Deepanshu123Bansal@users.noreply.github.com>
2023-03-25 00:49:04 +0530

    Create Ascll_value.c

---
+++ Ascll_value.c
@@ -0,0 +1,34 @@
+//Upper-case to Lower-case
+#include<stdio.h>
+int main()
+{
+  char ch;
+  printf("enter ch");
+  scanf("%c",&ch)//
+  ch=ch+32;
+  printf("%c",ch);
+  return 0;
+}
+  
+//Lowercase to Uppercase
+#include<stdio.h>
+int main()
+{
+  char ch;
+  printf("enter ch");
+  scanf("%c",&ch)//
+  ch=ch-32;
+  printf("%c",ch);
+  return 0;
+}
+
+//number to ASCLL value
+#include<stdio.h>
+int main()
+{
+ int n;
+  printf("enter ch");
+  scanf("%d",&n);
+  printf("%c",n);
+  return 0;
+}
