c68884c17e2e8264fec98f19630dd1d912e82214
Deepanshu123Bansal <122732006+Deepanshu123Bansal@users.noreply.github.com>
2023-03-25 23:07:19 +0530

    Create interger check whether number is greater than 10 or not.c

---
+++ interger check whether number is greater than 10 or not.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("enter n");
+  scanf("%d",&n);
+  if(n>10)
+  {
+    printf("Greater");
+  }
+  else
+  {
+    printf("less");
+  }
+  return 0;
+}
+         
