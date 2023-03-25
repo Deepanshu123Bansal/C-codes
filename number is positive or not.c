07f6afd3881c4e86639957ce770291feba42eb81
Deepanshu123Bansal <122732006+Deepanshu123Bansal@users.noreply.github.com>
2023-03-25 23:10:19 +0530

    Create number is positive or not.c

---
+++ number is positive or not.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("enter n");
+  scanf("%d",&n);
+  if(n>0)
+  {
+    printf("positive");
+  }
+  else if(n<0)
+  {
+    printf("negative");
+  }
+  else
+  {
+    printf"equal to zero");
+  }
+  return 0;
+}
