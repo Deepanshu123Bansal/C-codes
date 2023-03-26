22d901ca6a29b02c89c3220c2a20866eeaecada9
Deepanshu123Bansal <122732006+Deepanshu123Bansal@users.noreply.github.com>
2023-03-27 01:28:36 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+  int n,rem,reverse=0,t;
+  printf("enter n");
+  scanf("%d",&n);
+  t=n;
+  while(n>0)
+  {
+    rem=n%10;
+    reverse=reverse*10+rem;
+    n=n/10;
+  }
+  
+  if(reverse==t)
+  {
+    printf("palindrome number");
+  }
+  else
+  {
+    printf("not palindrome");
+  }
+  return 0;
+}
